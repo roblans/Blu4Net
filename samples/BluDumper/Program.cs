809ed2f9710ccbba43efdd2bbeb3f5ab3a4c737b
Rob Lans <r.lans@upcmail.nl>
2020-07-29 11:34:27 +0200

    Implemented DumpPlayer

--- samples/BluDumper/Program.cs
+++ samples/BluDumper/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Blu4Net;
+using System;
+using System.Net;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace BluDumper
 {
@@ -6,7 +10,42 @@ namespace BluDumper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (BluEnvironment.ResolveEndpoints()
+                .Select(async endPoint => await DumpEndpoint(endPoint))
+                .Subscribe())
+            {
+                Console.ReadLine();
+            }
+        }
+
+
+        private static async Task DumpEndpoint(Uri endpoint)
+        {
+            Console.WriteLine($"Endpoint: {endpoint}");
+
+            var player = await BluPlayer.Connect(endpoint);
+            await DumpPlayer(player);
+        }
+
+        private static async Task DumpPlayer(BluPlayer player)
+        {
+            Console.WriteLine($"Player: {player.Name}");
+            Console.WriteLine(new string('=', 80));
+
+            Console.WriteLine($"State: {player.State}");
+            Console.WriteLine($"Mode: {player.Mode}");
+            Console.WriteLine($"Volume: {player.Volume}%");
+            
+            Console.WriteLine($"Media:");
+            for(var i=0; i<player.Media.Titles.Length; i++)
+            {
+                Console.WriteLine($"\tTitle{i+1}: {player.Media.Titles[i]}");
+            }
+            Console.WriteLine($"\tImageUri: {player.Media.ImageUri}");
+            Console.WriteLine($"\tServiceIconUri: {player.Media.ServiceIconUri}");
+
+            Console.WriteLine(new string('=', 80));
+            Console.WriteLine();
         }
     }
 }
