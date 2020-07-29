577d5b7fa70aed0a3aa89e89df7daba1c83964f0
Rob Lans <r.lans@upcmail.nl>
2020-07-29 11:05:54 +0200

    Added sample project BluDumper

---
+++ samples/BluDumper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BluDumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
