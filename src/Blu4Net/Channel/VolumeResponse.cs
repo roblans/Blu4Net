c7ef79bff495aa819e8e67f949e9739c92a5c19d
Rob Lans <r.lans@upcmail.nl>
2020-08-05 09:02:12 +0200

    Workaround for: https://github.com/dotnet/runtime/issues/22656

--- src/Blu4Net/Channel/VolumeResponse.cs
+++ src/Blu4Net/Channel/VolumeResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Xml.Serialization;
 
@@ -14,8 +15,17 @@ namespace Blu4Net.Channel
         [XmlAttribute("mute")]
         public int Mute;
 
+#if NETSTANDARD2_0
+        // workaround for: XmlSerializer works inconsistently with XmlTextAttribute on derived classes
+        // https://github.com/dotnet/runtime/issues/22656
+        [XmlText()]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public string VolumeInternal;
+        public int Volume => int.Parse(VolumeInternal);
+#else
         [XmlText()]
         public int Volume;
+#endif
 
         public override string ToString()
         {
