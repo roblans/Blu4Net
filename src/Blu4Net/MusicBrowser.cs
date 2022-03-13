3add2e13c77455593d87df03584201a8c4b597ef
Colin Wilmans <colin.wilmans@floriade.com>
2022-03-13 13:58:14 +0100

    Generic entry for (self constructed) browse keys.

--- src/Blu4Net/MusicBrowser.cs
+++ src/Blu4Net/MusicBrowser.cs
@@ -18,6 +18,12 @@ namespace Blu4Net
             _channel = channel ?? throw new ArgumentNullException(nameof(channel));
         }
 
+        public async Task<MusicContentNode> BrowseContent(string key)
+        {
+            var content = await _channel.BrowseContent(key).ConfigureAwait(false);
+            return new MusicContentNode(_channel, this, content);
+        }
+
         public Task PlayURL(string playURL)
         {
             return _channel.PlayURL(playURL);
